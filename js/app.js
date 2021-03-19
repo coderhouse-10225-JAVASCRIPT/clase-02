c0f7c88f093fcc2ec8fd037e30006d3c9185ebb7
alejandro medici <el.aleyo@gmail.com>
2021-03-18 21:18:26 -0300

    Primero archivos de clase 02

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let unNumero = 5;
+
+if ( unNumero > 10) {
+    alert("Numero alto");
+} else {
+    alert("Numero menor a 10")
+}
+
+let nombre = prompt("Ingrese su nombre");
+
+if (nombre == "") {
+    alert("Che ingresa nombre!");
+} else {
+    alert("Hola señor " + nombre);
+}
+
+/* 
+   Quiero ingresar un numero por prompt
+   ver si el numero esta entre 18, 30 o es mayor 60
+*/ 
+
+let myAge = prompt("Ingrese su edad")
+
